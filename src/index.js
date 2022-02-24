bd1f57b473e9b025e5b9d310470105b935bb3e39
AnatoliyIliev <vekok12@gmail.com>
2022-02-24 21:16:34 +0200

    human rn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+  let dozens = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  let doz = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  let numberStr = number.toString();
+  if (number === 0) {
+    return 'zero';
+  } else if (number < 20) {
+    return dozens[number];
+  } else if (number < 100) {
+    return `${doz[numberStr.charAt(0)]} ${dozens[numberStr.charAt(1)]}`.trim();
+  } else if (number > 99 && number % 100 != 0) {
+    return `${dozens[numberStr.charAt(0)] + ' hundred'} ${toReadable(
+      number % 100,
+    )}`.trim();
+  } else {
+    return `${dozens[numberStr.charAt(0)] + ' hundred'}`;
+  }
+};
