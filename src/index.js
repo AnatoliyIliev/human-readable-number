39bbacd517c6fab86135b0456f98d2d886fd4a76
AnatoliyIliev <vekok12@gmail.com>
2022-02-28 12:54:50 +0200

    second solution

--- src/index.js
+++ src/index.js
@@ -34,18 +34,44 @@ module.exports = function toReadable(number) {
     'ninety',
   ];
 
-  let numberStr = number.toString();
+  const numToString = number.toString();
+
   if (number === 0) {
     return 'zero';
   } else if (number < 20) {
     return dozens[number];
-  } else if (number < 100) {
-    return `${doz[numberStr.charAt(0)]} ${dozens[numberStr.charAt(1)]}`.trim();
-  } else if (number > 99 && number % 100 != 0) {
-    return `${dozens[numberStr.charAt(0)] + ' hundred'} ${toReadable(
-      number % 100,
-    )}`.trim();
-  } else {
-    return `${dozens[numberStr.charAt(0)] + ' hundred'}`;
+  } else if (number > 19 && number < 100) {
+    return `${doz[numToString[0]]} ${dozens[numToString[1]]}`.trim();
+  } else if (number >= 100 && number < 1000) {
+    if (number % 100 < 20) {
+      return `${dozens[numToString[0]] + ' hundred'} ${
+        dozens[number % 100]
+      }`.trim();
+    } else {
+      return `${dozens[numToString[0]] + ' hundred'} ${doz[numToString[1]]} ${
+        dozens[numToString[2]]
+      }`.trim();
+    }
   }
 };
+
+// console.log(toReadable(100));
+//
+
+//
+//
+//   let numberStr = number.toString();
+//   if (number === 0) {
+//     return 'zero';
+//   } else if (number < 20) {
+//     return dozens[number];
+//   } else if (number < 100) {
+//     return `${doz[numberStr.charAt(0)]} ${dozens[numberStr.charAt(1)]}`.trim();
+//   } else if (number > 99 && number % 100 != 0) {
+//     return `${dozens[numberStr.charAt(0)] + ' hundred'} ${toReadable(
+//       number % 100,
+//     )}`.trim();
+//   } else {
+//     return `${dozens[numberStr.charAt(0)] + ' hundred'}`;
+//   }
+// };
